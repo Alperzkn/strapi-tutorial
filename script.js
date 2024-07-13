a3a24a14760f41f5753a082d1f7a0aa0a5486311
Alperzkn <20475676+Alperzkn@users.noreply.github.com>
2024-07-13 09:54:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 async function getRestaurants() {
   const response = await fetch("http://127.0.0.1:1337/api/restaurants");
   //console.log(response);
-  data = await response.json();
-  //console.log(data);
-  restaurants = data.data;
+  rawData = await response.json();
+  //console.log(rawData);
+  restaurants = rawData.data;
   console.log(restaurants.length);
 
   const restaurantsRow = document.getElementById("restaurants");
@@ -17,7 +17,7 @@ async function getRestaurants() {
 
     const HTMLData = `
 
-    <h1 s>${restaurantName}</h1>
+    <h1>${restaurantName}</h1>
     <p>${restaunrantDescription}</p>
     <p>${restaurantCreatedAt}</p>
     <hr>
